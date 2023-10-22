074aa037e7c8ff94f8c575ffdc102774401a1c38
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-10-23 03:28:06 +0530

    implement header and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomePage from './pages/HomePage';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Header />
+      <main className='container'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
